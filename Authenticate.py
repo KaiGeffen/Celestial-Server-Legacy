256995f5bcdeee3c27a7ea076813d574d2d6f63c
Kai Geffen <geffenkai@gmail.com>
2021-10-14 15:20:14 -0400

    Added sql commands for opening a pack and choosing a card from a pack"

--- Authenticate.py
+++ Authenticate.py
@@ -28,8 +28,8 @@ async def authenticate(ws):
 
         # Data about this user, stored locally for session
         user_data = None
-        # The last pack this user opened
-        pack = None
+        # The last set of choice cards user saw
+        choice_cards = None
 
         if data["type"] == "send_token":
             token = data['value']
@@ -54,14 +54,19 @@ async def authenticate(ws):
                 pack = get_random_pack()
 
                 # Subtract funds, add these cards to user inventory
-                adjust_user_data_opened_pack(id, pack)
+                # The first card of the choosables is picked by default,
+                # then if another is chosen, the inventory is adjusted
+                adjust_user_data_opened_pack(uuid, pack)
+
+                choice_cards = pack[4:]
 
                 message = json.dumps({"type": "pack", "value": pack})
             await asyncio.wait([ws.send(message)])
         elif data["type"] == "make_choice":
-            # Adjust the inventory to reflect if user chose a card besides the first option
-            pass
+            chosen_card = choice_cards[data['value']]
 
+            # Adjust the inventory to reflect if user chose a card besides the first option
+            adjust_user_data_chosen_card(chosen_card, choice_cards[0])
 
 
 
@@ -93,7 +98,6 @@ def get_user_data(id, email):
         # Postgres uuid requires 32 (hex) digits, so add trailing 0s to get that many digits
         num_digits = len(str(id))
         padded_id = str(id) + '0' * (32 - num_digits)
-        print(padded_id)
         select_query = f"SELECT * from players where id = '{padded_id}'"
         cursor.execute(select_query)
         count = cursor.rowcount
@@ -120,6 +124,63 @@ def get_user_data(id, email):
 
             print(f"Created the basic entry: {user_data}")
             return user_data
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+# For user with given id, add the given pack to their inventory
+def adjust_user_data_opened_pack(uuid, pack):
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="doadmin",
+                                      password=os.environ["DB_PWD"],
+                                      host="app-8058d91d-8288-43bb-a12e-e1eb61ce00e3-do-user-8861671-0.b.db.ondigitalocean.com",
+                                      port="25060",
+                                      database="defaultdb",
+                                      sslmode="require")
+
+        cursor = connection.cursor()
+
+        update_query = "UPDATE players\n"
+        update_query += f"inventory[${pack[0]}] = coalesce(inventory[${pack[0]}], 0) + 1, inventory[${pack[1]}] = coalesce(inventory[${pack[1]}], 0) + 1, inventory[${pack[2]}] = coalesce(inventory[${pack[2]}], 0) + 1, inventory[${pack[3]}] = coalesce(inventory[${pack[3]}], 0) + 1, inventory[${pack[4]}] = coalesce(inventory[${pack[4]}], 0) + 1"
+        update_query += f"WHERE id = ${uuid};"
+
+        cursor.execute(update_query)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+# For user with given id, submit their choice (Remove the default chosen card, add the one they chose)
+def adjust_user_data_chosen_card(uuid, chosen_card, default_card):
+    if chosen_card == default_card:
+        return
+
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="doadmin",
+                                      password=os.environ["DB_PWD"],
+                                      host="app-8058d91d-8288-43bb-a12e-e1eb61ce00e3-do-user-8861671-0.b.db.ondigitalocean.com",
+                                      port="25060",
+                                      database="defaultdb",
+                                      sslmode="require")
+
+        cursor = connection.cursor()
+
+        update_query = "UPDATE players\n"
+        update_query += f"inventory[${chosen_card}] = coalesce(inventory[${chosen_card}], 0) + 1, inventory[${default_card}] = coalesce(inventory[${default_card}], 0) - 1"
+        update_query += f"WHERE id = ${uuid};"
+
+        cursor.execute(update_query)
 
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
