f9dd7434d6759990d7a2b822a068836155d709b6
Kai Geffen <geffenkai@gmail.com>
2022-06-01 23:26:07 -0400

    Changed Oak to give nourish equal to how much you win by

--- logic/ServerController.py
+++ logic/ServerController.py
@@ -304,7 +304,8 @@ class ServerController:
             # Subtract the safety of the losing player
             score_above_winning = score_dif
             # Subtract 1 because that much is needed to take the round
-            score_above_winning -= 1
+            # NOTE This has been changed
+            # score_above_winning -= 1
 
             if score_above_winning > 0 and Status.GENTLE in self.model.status[p1]:
                 self.model.status[p1].extend(score_above_winning * [Status.NOURISH])
